03582c9017920cefa52644fd350dcc6d5e67c265
Tommaso Girotto <tommaso.girotto91@gmail.com>
2019-11-11 10:19:21 +0300

    ready

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(session({
   }),
   secret: "hellogopamoja",
   resave: false,
-  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000},
+  cookie: { maxAge: 30 * 24 * 60 * 60 * 1000, domain:'.gopamoja.com', secure: true},
   saveUninitialized: false
 }));
 
