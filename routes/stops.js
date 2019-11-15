ddaf4b8b08853603140200a465773395c806a6d2
Tommaso Girotto <tommaso.girotto91@gmail.com>
2019-11-15 14:32:05 +0300

    ready

--- routes/stops.js
+++ routes/stops.js
@@ -2,18 +2,149 @@ const pg = require('../config/pg');
 const Router = require('express-promise-router');
 
 const StopService = require('../services/StopService');
+const UserService = require('../services/UserService');
 
 const router = new Router()
 
 router.get('/', async function(req, res, next) {
+  if(!req.session.user_id) {
+    res.status(401).json({response: 'Not Authorised.'});
+    return;
+  }
+
   try {
     const stops = await StopService.findAll();
-
     res.json({
       stops: stops
     })
   } catch(e) {
     res.status(500).json({e: e.toString()});
+  }
+});
+
+router.get('/:id', async function(req, res, next) {
+  if(!req.session.user_id) {
+    res.status(401).json({response: 'Not Authorised.'});
+    return;
+  }
+
+  try {
+    const stopId = parseInt(req.params.id)
+
+    if(isNaN(stopId)) {
+      throw "Stop id invalid"
+    }
+
+    const stop = await StopService.findById(stopId);
+    res.json({
+      stop: stop
+    })
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({err: e.toString()});
+  }
+});
+
+router.post('/', async function(req, res, next) {
+  if(!req.session.user_id) {
+    res.status(401).json({response: 'Not Authorised.'});
+    return;
+  }
+
+  const name = req.body.name;
+  const popular = req.body.popular;
+  const country = req.body.country;
+  const timezone = req.body.timezone;
+
+  try {
+    if(typeof name !== 'string') {
+      throw "name is invalid"
+    }
+
+    if(typeof country !== 'string') {
+      throw "country is invalid"
+    }
+
+    if(typeof timezone !== 'string') {
+      throw "timezone is invalid"
+    }
+
+    if(typeof popular !== 'boolean') {
+      throw "popular is invalid"
+    }
+
+    let user = await UserService.findById(req.session.user_id);
+
+    if(user == null || user.role == null) {
+      throw "User or user role not defined";
+    }
+
+    if(user.role === 'admin') {
+      const stop = await StopService.insertOne(name, popular, country, timezone);
+      res.json({
+        stop: stop
+      })
+    } else {
+      res.status(401).json({response: 'Not Authorised.'});
+      return;
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({err: e.toString()});
+  }
+});
+
+router.put('/:id', async function(req, res, next) {
+  if(!req.session.user_id) {
+    res.status(401).json({response: 'Not Authorised.'});
+    return;
+  }
+
+  try {
+    const stopId = parseInt(req.params.id);
+    const name = req.body.name;
+    const country = req.body.country;
+    const timezone = req.body.timezone;
+    const popular = req.body.popular;
+
+    if(isNaN(stopId)) {
+      throw 'Stop id is invalid'
+    }
+
+    if(typeof name !== 'string') {
+      throw 'Name is invalid'
+    }
+
+    if(typeof country !== 'string') {
+      throw 'country is invalid'
+    }
+
+    if(typeof timezone !== 'string') {
+      throw 'timezone is invalid'
+    }
+
+    if(typeof popular !== 'boolean') {
+      throw 'popular is invalid'
+    }
+
+    let user = await UserService.findById(req.session.user_id);
+
+    if(user == null || user.role == null) {
+      throw "User or user role not defined";
+    }
+
+    if(user.role === 'admin') {
+      const stop = await StopService.updateOne(stopId, name, popular, country, timezone);
+      res.json({
+        stop: stop
+      })
+    } else {
+      res.status(401).json({response: 'Not Authorised.'});
+      return;
+    }
+  } catch(e) {
+    console.log(e);
+    res.status(500).json({err: e.toString()});
   }
 });
 
--- services/StopService.js
+++ services/StopService.js
@@ -1,6 +1,6 @@
 const pg = require('../config/pg');
 
-const StopService = {
+const stopservice = {
   findAll: async () => {
     const client = await pg.connect()
     let result;
@@ -13,7 +13,7 @@ const StopService = {
       result = await client.query(q0);
 
       if(result == null || result.rows == null) {
-        throw "Stops get did not return any result";
+        throw "stops get did not return any result";
       }
 
       await client.query('COMMIT')
@@ -28,7 +28,145 @@ const StopService = {
     } finally {
       client.release()
     }
+  },
+  findById: async (stopId) => {
+    if(isNaN(stopId)) {
+      throw "Invalid stop id"
+    }
+
+    const client = await pg.connect()
+    let result;
+
+    try {
+      await client.query('BEGIN')
+
+      let q0 = `select * from stops where id = $1`;
+
+      result = await client.query(q0, [stopId]);
+
+      if(result == null || result.rows == null) {
+        throw "stops get did not return any result";
+      }
+
+      if(result.rows.length < 1) {
+        throw "stop not found";
+      }
+
+      await client.query('COMMIT')
+
+      return new Promise((resolve, reject) => {
+        resolve(result.rows[0]);
+      });
+    } catch(e) {
+      await client.query('ROLLBACK')
+      throw e;
+    } finally {
+      client.release()
+    }
+  },
+  insertOne: async (name, popular, country, timezone) => {
+    if(typeof name !== 'string') {
+      throw "name is invalid"
+    }
+
+    if(typeof country !== 'string') {
+      throw "country is invalid"
+    }
+
+    if(typeof timezone !== 'string') {
+      throw "timezone is invalid"
+    }
+
+    if(typeof popular !== 'boolean') {
+      throw "popular is invalid"
+    }
+
+    const client = await pg.connect()
+    let result;
+
+    try {
+      await client.query('BEGIN')
+
+      let q0 = `insert into stops(name, popular, country, timezone) values ($1, $2, $3, $4) returning *`;
+
+      result = await client.query(q0, [name, popular, country, timezone]);
+
+      if(result == null || result.rows == null) {
+        throw "stop insert did not return any result";
+      }
+
+      if(result.rows.length < 1) {
+        throw "stop insert did not return any result";
+      }
+
+      await client.query('COMMIT')
+
+      return new Promise((resolve, reject) => {
+        resolve(result.rows[0]);
+      });
+    } catch(e) {
+      await client.query('ROLLBACK')
+      throw e;
+    } finally {
+      client.release()
+    }
+  },
+  updateOne: async (stopId, name, popular, country, timezone) => {
+    if(isNaN(stopId)) {
+      throw 'Stop id is invalid'
+    }
+
+    if(typeof name !== 'string') {
+      throw 'Name is invalid'
+    }
+
+    if(typeof country !== 'string') {
+      throw 'country is invalid'
+    }
+
+    if(typeof timezone !== 'string') {
+      throw 'timezone is invalid'
+    }
+
+    if(typeof popular !== 'boolean') {
+      throw 'popular is invalid'
+    }
+
+    const client = await pg.connect()
+    let result;
+
+    try {
+      await client.query('BEGIN')
+
+      let q0 = `update stops set name = $1, \
+       popular = $2, \
+       country = $3, \
+       timezone = $4 \
+       where id = $5
+       returning *`;
+
+      result = await client.query(q0, [name, popular, country, timezone, stopId]);
+
+      if(result == null || result.rows == null) {
+        throw "Stop update did not return any result";
+      }
+
+      if(result.rows.length < 1) {
+        throw "Stop update did not return any result";
+      }
+
+      await client.query('COMMIT')
+
+      return new Promise((resolve, reject) => {
+        resolve(result.rows[0]);
+      });
+    } catch(e) {
+      await client.query('ROLLBACK')
+      throw e;
+    } finally {
+      client.release()
+    }
   }
 }
 
-module.exports = StopService;
+module.exports = stopservice;
